c158df268fa6bfe113f7071ac66f158eefe549d3
Marco Rossel <andresmarcorossel@gmail.com>
2024-07-26 09:02:10 -0400

    proj piramide

--- algorithms_and_data_structures/00.js
+++ algorithms_and_data_structures/00.js
@@ -1,1 +1,42 @@
-/* 2024 */
+/* 2024
+
+JavaScript has seven primitive data types
+
+When you declare a variable without initializing it, it is considered uninitialized. Currently, your secondCharacter variable is uninitialized.
+let -- cuando declaramos una variable sin inicializar esta indefinida (undefined)
+--------------------------------------
+18
+
+An array is a non-primitive data type that can hold a series of values. Non-primitive data types differ from primitive data types in that they can hold more complex data. Primitive data types like strings and numbers can only hold one value at a time
+
+array no es un tipo de dato primitivo
+se diferencia de un primitovo ----primitivo = solo un tipo de valor "string, boolean, number"
+----non-primitive = puedes guardar cualquier tipo de valor, incluido un propio non-primitive value
+--------------------------------------
+27  metodos de un array
+
+array.push() agrega un valor al findal de un array
+array.pop() elimina el valor findal de un array pero ojo que lo devuelve, por lo que lo puedes guardar en una variable para utilizarlo mas tarde
+
+si guardamos en una variable lo que pusheamos array.push() nos retornara un numero con la longitud nueva del array
+--------------------------------------
+33  loop sintax (for loop)
+
+for (iterator; condition; iteration) {
+  logic;
+}
+
+for ([es const por que cambia en cada iteracion]  of [array a iterar]) { en el body esta la logica a aplicar value}
+
+for (const value of iterable) {
+
+}
+--------------------------------------
+54 functions
+
+function demo(parameter){
+
+}
+demo(argument)
+
+*/
