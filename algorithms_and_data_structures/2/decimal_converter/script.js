e2aa7eb6c29557c6d12a14b15fd6af16e2babb2d
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-07 01:45:04 -0400

    3 unit | convertidor de decimal a binomial proj | 2

--- algorithms_and_data_structures/2/decimal_converter/script.js
+++ algorithms_and_data_structures/2/decimal_converter/script.js
@@ -1,3 +1,23 @@
+const callStack = [];
+const a = () => {
+  return "freeCodeCamp " + b();
+}
+const b = () => {
+  return "is " + c();
+}
+const c = () => {
+  return "awesome!";
+}
+console.log(a());
+
+/* Step 47
+In computer science, a stack is a data structure where items are stored in a LIFO (last-in-first-out) manner. If you imagine a stack of books, the last book you add to the stack is the first book you can take off the stack. Or an array where you can only .push() and .pop() elements.
+
+The call stack is a collection of function calls stored in a stack structure. When you call a function, it is added to the top of the stack, and when it returns, it is removed from the top / end of the stack.
+
+You'll see this in action by creating mock call stack.
+
+Initialize a variable named callStack and assign it an empty array. */
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
@@ -24,6 +44,15 @@ const decimalToBinary = (input) => {
   result.innerText = remainders.reverse().join(""); */
   // logica más eficiente.
   let binary = "";
+  if (input === 0) {
+    binary = "0";
+    return;
+  }
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input /2);
+  }
+  result.innerText = binary;
 };
 
 const checkUserInput = () =>{
