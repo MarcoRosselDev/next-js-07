3d168ef4f676edabd10778339ad8747af8633f30
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-11 01:53:08 -0400

    3 unit | statistics calculator

--- algorithms_and_data_structures/3/sort/script.js
+++ algorithms_and_data_structures/3/sort/script.js
@@ -1,42 +1,11 @@
-// bubble algorith
-/* const sortButton = document.getElementById("sort");
-
-const sortInputArray = (event) => {
-  event.preventDefault();
-  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-  const sortedValues = bubbleSort(inputValues);
-  updateUI(sortedValues);
-}
-const updateUI = (array=[]) => {
-  array.forEach((num, i) => {
-    const outputValueNode = document.getElementById(`output-value-${i}`);
-    outputValueNode.innerText = num;
-  })
-}
-
-const bubbleSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1; j++) {
-      console.log(array, array[j], array[j + 1]);
-      if (array[j] > array[j + 1]) {
-        const temp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temp;
-      }
-    }
-  }
-  return array;
-}
-const selectionSort = (array) => {};
-
-sortButton.addEventListener("click", sortInputArray); */
-// sort algorith
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
   const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-  const sortedValues = selectionSort(inputValues);
+  const sortedValues = inputValues.sort((a, b) => {
+    return a-b;
+  });
   updateUI(sortedValues);
 }
 const updateUI = (array=[]) => {
@@ -72,6 +41,18 @@ const selectionSort = (array) => {
     const temp = array[i];
     array[i] = array[minIndex];
     array[minIndex] = temp;
+  }
+  return array;
+};
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i-1;
+    while (j >= 0 && array[i] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
   }
   return array;
 };
