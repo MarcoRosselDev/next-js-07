e434438549b2a34872b508c74c0ed5c64104379f
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-13 18:53:37 -0400

    3 unit | proyecto final | testing regex input filter

--- algorithms_and_data_structures/3/telephon_number_validator/script.js
+++ algorithms_and_data_structures/3/telephon_number_validator/script.js
@@ -1,6 +1,14 @@
-const regex = /1?\s?([0-9][0-9][0-9])?-?\s?[0-9][0-9][0-9]\s?-?[0-9][0-9][0-9][0-9]/gi; // este es el unico formato asepatado
+//const regex = /1?\s?\(?[0-9][0-9][0-9]\)?-?\s?[0-9][0-9][0-9]\s?-?[0-9][0-9][0-9][0-9]/gi; // este es el unico formato asepatado
+const a = "(1\s)?";
+const b = "(\([0-9][0-9][0-9]\)|[0-9][0-9][0-9])";
+const dashSpace = "([\s-]?)";
+const c = "([0-9][0-9][0-9])"
+const d = "([0-9][0-9][0-9][0-9])"
+//const finalRegex = /`${a}${b}${dashSpace}${c}${dashSpace}${d}`/g;
+//const finalRegex = /^(1\s)?(\([0-9][0-9][0-9]\)|[0-9][0-9][0-9])([\s-]?)([0-9][0-9][0-9])([\s-]?)([0-9][0-9][0-9])([\s-]?)([0-9][0-9][0-9][0-9])$/;
+const finalRegex = /^(1\s)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+console.log(finalRegex);
 
-const test = str => str.match(regex)
-const regexStr = str => str.
+const testa = str => finalRegex.test(str)
 
-console.log(test("(555)555-5555"));
+console.log(testa("1 555-555-5555"));
