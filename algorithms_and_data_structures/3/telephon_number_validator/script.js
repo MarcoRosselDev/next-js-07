bd44436bbbe18b7b4c221fa4daf2f3581785041b
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-13 07:37:55 -0400

    3 unit | proyecto final 2

--- algorithms_and_data_structures/3/telephon_number_validator/script.js
+++ algorithms_and_data_structures/3/telephon_number_validator/script.js
@@ -1,1 +1,6 @@
-const regex = /1?\d?\(?[0-9][0-9][0-9])?-?\d?[0-9][0-9][0-9]\d?-?[0-9][0-9][0-9][0-9]/; // este es el unico formato asepatado
+const regex = /1?\s?([0-9][0-9][0-9])?-?\s?[0-9][0-9][0-9]\s?-?[0-9][0-9][0-9][0-9]/gi; // este es el unico formato asepatado
+
+const test = str => str.match(regex)
+const regexStr = str => str.
+
+console.log(test("(555)555-5555"));
