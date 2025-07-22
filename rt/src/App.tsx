b2c80c73a3ade66199319dd8d829f1d18eb19bdb
Marco Rossel <andresmarcorossel@gmail.com>
2025-07-22 18:34:31 -0400

    react | formularios no controlados

--- rt/src/App.tsx
+++ rt/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @format */
+
+/* ejemplo de formulario no controlado
+
+constrolado y no controlado significa que utilizamos o no el estado de los inputs
+para mostrar inmediatamente algo al usuario mientras esta completando el form
+el form no controlado es util para forms largos donde se hace muy pasado mantener el 
+estado de todos los inputs.
+el controlado es util para mostrar lo que se esta ingresando.
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form>
+      <input type='text' name='tutum-sahur' />
+      <button type='submit'>sumbit</button>
+    </form>
+  );
 }
 
-export default App
+este codigo simple envia los datos form a una url que no se encuentra  
+por lo tanto se envian aqui mismo:
+  http://localhost:5173/?tutum-sahur=aaa
+
+*/
+
+import { useRef, type FormEvent } from "react";
+import "./App.css";
+
+function App() {
+  const miReff = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // the ? esta preguntndo si existe current entonce acceder a .value
+    const data = miReff.current?.value; 
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='tutum-sahur' ref={miReff} />
+      <button type='submit'>sumbit</button>
+    </form>
+  );
+}
+
+export default App;
