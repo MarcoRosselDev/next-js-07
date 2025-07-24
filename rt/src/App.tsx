d35444214a73fdf46e8cdede8c840d2f9806b99c
Marco Rossel <andresmarcorossel@gmail.com>
2025-07-23 23:01:32 -0400

    react | formularios no controlados | new FromData

--- rt/src/App.tsx
+++ rt/src/App.tsx
@@ -5,18 +5,37 @@ import "./App.css";
 
 function App() {
 
-  // input ref
-  const iref = useRef<HTMLInputElement>(null);
-
+  const formRef = useRef(null)
+  
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    const input = iref.current?.value;
-    console.log(input);
+    if (!formRef.current) return
+    const formData = new FormData(formRef.current)
+
+    const color = formData.get("color")
+    const inpt = formData.get("patapin")
+    const terminos = formData.get("terminos")
+    
+    console.log(color,inpt, terminos);
+    console.log({color,inpt, terminos});
+    
+    //const input = iref.current?.value; // optional chaining operator | es | Operador de encadenamiento opcional
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="patapin" ref={iref} />
+    <form onSubmit={handleSubmit} ref={formRef}>
+      <input type="text" name="patapin"  />
+
+      {/* opciones de color */}
+      <select name="color" defaultValue={""}>
+        <option value="" disabled>seleccióna un color</option>
+        <option value="red">rojo</option>
+        <option value="green">verde</option>
+        <option value="blue">azul</option>
+      </select>
+      {/* checkbox aceptar terminos */}
+        <label htmlFor="ch">Aceptar terminos y condiciones.</label>
+        <input type="checkbox" id="ch" name="terminos"/>
       <button type="submit">send</button>
     </form>
   )
