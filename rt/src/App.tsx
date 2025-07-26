ae80dd2c0496cf409ad8199106c6b0e9a0bfaf49
Marco Rossel <andresmarcorossel@gmail.com>
2025-07-26 05:20:13 -0400

    react | formularios controlados | tailwind css whit vite

--- rt/src/App.tsx
+++ rt/src/App.tsx
@@ -1,60 +1,12 @@
-/** @format */
-import { useState, type FormEvent } from "react";
-import "./App.css";
+import './App.css'
+import Example from './Example'
 
-// controlled components (useState to do that)
-function App() {  
-  // estados controlados (useState)
-  const [color, setcolor] = useState("")
-  const [ch, setch] = useState(false)
-  const [name, setname] = useState("")
+function App() {
 
-  const hs = (e: FormEvent) => {
-    e.preventDefault()
-    console.log(color, ch, name);
-  }
+  return <div className="relative grid min-h-screen">
 
-  return (
-    <>
-      {/* veneficio:
-          podemos realizar validaciones inmediatas cuando el usuario este escribiendo en un input
-          por ejemplo si el lenght es menor a 6 le enviamos un mensaje inmediato de que escriba mas caracteres
-          tambien podemos validar el correo valido
-          o podemos relisar una busqueda en la db mientras esta tipeando.
-      */}
-      <form onSubmit={hs}>
-        <label>
-          name:
-          <input type="text" onChange={e => setname(e.target.value)} value={name} />
-        </label>
-        <label>
-          favorite color:
-          <select onChange={e => setcolor(e.target.value)} value={color}>
-            <option disabled value="">select ... </option>
-            <option value="red" >red</option>
-            <option value="blue" >blue</option>
-            <option value="green" >green</option>
-          </select>
-        </label>
-        <label>
-          accept terms
-          {/* value={ch} xxx el type checkbox acepta checked en su lugar
-          vendria siendo el unico type input diferente */}
-          <input 
-          type="checkbox" 
-          checked={ch}
-          onChange={() => setch(prev => !prev)}
-          />
-        </label>
-        <button type="submit">send</button>
-      </form>
-      <div>
-        <p>name: {name}</p>
-        <p>color: {color}</p>
-        <p>accept: {ch.toString()}</p>
-      </div>
-    </>
-  )
+    <Example />
+  </div>
 }
 
-export default App;
+export default App
