2dceb1984b47ed6af095dd9e302da6852e3da050
Marco Rossel <andresmarcorossel@gmail.com>
2025-07-29 21:48:52 -0400

    react | formularios controlados | bootstrap css whit vite and react third try success

--- rt/src/App.tsx
+++ rt/src/App.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import MyForm from './MyForm'
 
 function App() {
+  const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+    // Initialize theme from localStorage or default to 'light'
+    return localStorage.getItem('theme') as 'light' | 'dark' || 'light';
+  });
+
+  useEffect(() => {
+    // Apply the theme to the HTML element
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    // Save the theme preference to localStorage
+    localStorage.setItem('theme', theme);
+  }, [theme]); // Rerun effect when theme changes
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
 
   return (
     <>
-      <div className="container py-4 px-3 mx-auto">
-      <h1>Hello, Bootstrap and Vite!</h1>
-      <button className="btn btn-primary">Primary button</button>
-    </div>
+      <header>
+        <button className={`btn btn-sm ${theme === 'light'? 'btn-dark' : 'btn-outline-light' }`} onClick={toggleTheme}>
+        {theme === 'light' ? 'Dark' : 'Light'} theme
+        </button>
+      </header>
+        <p className="mt-3">
+          This is some content that will change appearance with the theme.
+        </p>
+      <MyForm />
     </>
   )
 }
