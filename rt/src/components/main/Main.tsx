c66d89d0e65476d64bfeb5d8f33721ce8cbab06b
Marco Rossel <andresmarcorossel@gmail.com>
2025-08-03 12:15:16 -0400

    react | formularios controlados | refact v3

--- rt/src/components/main/Main.tsx
+++ rt/src/components/main/Main.tsx
@@ -1,9 +1,26 @@
-import MyForm from "./MyForm";
+//import MyForm from "./MyForm";
+import Check from "../../components/main/Authocheck"
 
 export default function Main() {
   return (
     <main className="container-sm">
-      <MyForm />
+      <Check />
+      {/* <MyForm />
+      <p>another form</p>
+      <form className="">
+        <div className="mb-3 row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" id="staticEmail"  />
+          </div>
+        </div>
+        <div className="mb-3 row">
+          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="inputPassword" />
+          </div>
+        </div>
+      </form> */}
     </main>
   )
 }
